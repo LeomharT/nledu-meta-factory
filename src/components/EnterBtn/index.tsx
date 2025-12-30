f674b5e13fdf5099597f4dda45d3221b2b731d63
LeomharT <leo.liao.lzy@gmail.com>
2025-12-30 11:25:27 +0800

    feat: enter btn

--- src/components/EnterBtn/index.tsx
+++ src/components/EnterBtn/index.tsx
@@ -1,3 +1,10 @@
+import { ArrowRightOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+import classes from './style.module.css';
 export default function EnterBtn() {
-  return <div>EnterBtn</div>;
+  return (
+    <Button className={classes.btn} block icon={<ArrowRightOutlined />}>
+      进入教学实验平台
+    </Button>
+  );
 }
